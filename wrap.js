03611c69576579c7e289fa9be7bd16338fe03b11
Nirmal Lankathilaka <theShayn+nir@gmail.com>
2014-12-26 18:23:51 +0530

    Make slides transitionable

---
+++ wrap.js
@@ -0,0 +1,34 @@
+var slideId=0,
+    slideMax=-1,
+    slides
+;
+
+(function (){
+    
+    // set iterative IDs for the slides(wrappers)
+    slides=document.getElementsByClassName('slide-wrapper');
+    for (var i=0; i<slides.length; i++) {
+        slides[i].setAttribute('id', i);
+        slideMax++;
+    }
+    
+    // set first slide
+    slides[slideId].classList.add('current-slide');
+    
+    // track arrow-keys and change slides
+    document.onkeydown = function (e) {
+        e = e || window.event;
+        if (e.keyCode=='39') { // right
+            if (slideId<slideMax) {
+                slides[slideId].classList.remove('current-slide');
+                slideId++;
+                slides[slideId].classList.add('current-slide');
+            } else {
+                slides[slideId].classList.remove('current-slide');
+                slideId=0;
+                slides[slideId].classList.add('current-slide');
+            }
+        }
+    }
+    
+})();
